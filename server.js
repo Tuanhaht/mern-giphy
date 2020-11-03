df07eff02eb04f02315d2a0d23fbc46003c43090
Tran Van Tuan <tuanha10a4@gmail.com>
2020-11-03 13:14:51 +0700

    fix cors

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(
 );
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
